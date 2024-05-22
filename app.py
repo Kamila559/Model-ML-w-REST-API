13896be38e11b2d02f79ef29fd09a296a1367a45
Kamila559 <153556615+Kamila559@users.noreply.github.com>
2024-05-22 23:40:29 +0200

    app.py

--- app.py
+++ app.py
@@ -32,26 +32,7 @@ class Perceptron():
 # Create a flask
 app = Flask(__name__)
 
-# Create an API end point
-@app.route('/predict_get', methods=['GET'])
-def get_prediction():
-    # sepal length
-    sepal_length = float(request.args.get('sl'))
-    petal_length = float(request.args.get('pl'))
-    
-    features = [sepal_length, petal_length]
-
-    # Load pickled model file
-    with open('model.pkl',"rb") as picklefile:
-        model = pickle.load(picklefile)
-        
-    # Predict the class using the model
-    predicted_class = int(model.predict(features))
-    
-    # Return a json object containing the features and prediction
-    return jsonify(features=features, predicted_class=predicted_class)
-
-@app.route('/predict_post', methods=['POST'])
+@app.route('/', methods=['POST'])
 def post_predict():
     # sepal length
     sepal_length = 6.3
