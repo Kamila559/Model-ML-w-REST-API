c4122a440fff285beaf14986d9ca1e232a69e739
Kamila559 <153556615+Kamila559@users.noreply.github.com>
2024-05-22 22:32:25 +0200

    app.py

--- app.py
+++ app.py
@@ -1,47 +1,74 @@
-from flask import Flask, request
+import pickle
 import numpy as np
-import pandas as pd
-from sklearn.datasets import load_iris
-
-app = Flask(__name__)
+from flask import Flask, request, jsonify
 
 class Perceptron():
-    def __init__(self, n_iter=10, eta=0.01):
-        self.n_iter = n_iter
+    
+    def __init__(self, eta=0.01, n_iter=10):
         self.eta = eta
-        
+        self.n_iter = n_iter
+    
     def fit(self, X, y):
-        self.w_ = np.zeros(1+X.shape[1])
+        self.w_ = [random.uniform(-1.0, 1.0) for _ in range(1+X.shape[1])] 
         self.errors_ = []
+        
         for _ in range(self.n_iter):
             errors = 0
+            for xi, target in zip(X,y):
+                update = self.eta*(target-self.predict(xi))
+                self.w_[1:] += update*xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
         return self
-
-    def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1, -1)
-
-    def net_input(self, X):
-        return np.dot(X, self.w_[1:]) + self.w_[0]
-
-def read_data():
-    iris = load_iris()
-    df = pd.DataFrame(data= np.c_[iris['data'], iris['target']], columns= iris['feature_names'] + ['target'])
-    X = df.iloc[:100,[0,2]].values
-    y = df.iloc[0:100,4].values
-    y = np.where(y == 0, -1, 1)
-    return X, y
-
-@app.route('/', methods=['POST'])
-def fit_perceptron():
-    X, y = read_data()
     
-    perceptron = Perceptron()
-    perceptron.fit(X, y)
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:])+self.w_[0]
+    
+    def predict(self, X):
+        return np.where(self.net_input(X)>=0.0, 1, -1)
 
-    weights = perceptron.w_
-    errors = perceptron.errors_
+# Create a flask
+app = Flask(__name__)
 
-    return weights
+# Create an API end point
+@app.route('/predict_get', methods=['GET'])
+def get_prediction():
+    # sepal length
+    sepal_length = float(request.args.get('sl'))
+    petal_length = float(request.args.get('pl'))
+    
+    features = [sepal_length, petal_length]
+
+    # Load pickled model file
+    with open('model.pkl',"rb") as picklefile:
+        model = pickle.load(picklefile)
+        
+    # Predict the class using the model
+    predicted_class = int(model.predict(features))
+    
+    # Return a json object containing the features and prediction
+    return jsonify(features=features, predicted_class=predicted_class)
+
+@app.route('/predict_post', methods=['POST'])
+def post_predict():
+    data = request.get_json(force=True)
+    # sepal length
+    sepal_length = float(data.get('sl'))
+    petal_length = float(data.get('pl'))
+    
+    features = [sepal_length, petal_length]
+
+    # Load pickled model file
+    with open('model.pkl',"rb") as picklefile:
+        model = pickle.load(picklefile)
+        
+    # Predict the class using the model
+    predicted_class = int(model.predict(features))
+    output = dict(features=features, predicted_class=predicted_class)
+    # Return a json object containing the features and prediction
+    return jsonify(output)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
